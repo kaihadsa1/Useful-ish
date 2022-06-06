5ef0bc0c7ba7db21d9eac4cac5fe1c97bdc0320c
KaiHassan <kaihassan99@gmail.com>
2022-06-06 16:37:41 +0100

    Allows for purge (/purge [number])

---
+++ hikari-intro-main/lightbulb_bot/commands/moderation.py
@@ -0,0 +1,41 @@
+import asyncio
+
+import hikari
+import lightbulb
+from lightbulb import errors
+
+
+
+mod_plugin = lightbulb.Plugin("Mod")
+
+@lightbulb.add_checks(
+    lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES),
+    lightbulb.bot_has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES)
+)
+
+@mod_plugin.command
+@lightbulb.option(
+    "messages", "The number of messages to purge.", type=int, required=True
+)
+@lightbulb.command("purge", "Purge messages.", aliases=["clear"])
+@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+async def purge_messages(ctx: lightbulb.Context) -> None:
+    num_msgs = ctx.options.messages
+    channel = ctx.channel_id
+
+    # If the command was invoked using the PrefixCommand, it will create a message
+    # before we purge the messages, so you want to delete this message first
+    if isinstance(ctx, lightbulb.PrefixContext):
+        await ctx.event.message.delete()
+
+    msgs = await ctx.bot.rest.fetch_messages(channel).limit(num_msgs)
+    await ctx.bot.rest.delete_messages(channel, msgs)
+
+    resp = await ctx.respond(f"{len(msgs)} messages deleted")
+
+    await asyncio.sleep(5)
+    await resp.delete()
+
+
+def load(bot: lightbulb.BotApp) -> None:
+    bot.add_plugin(mod_plugin)
